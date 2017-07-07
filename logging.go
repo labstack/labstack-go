b73ad6061f33dbdd081607a668dec9c7b74022eb
Vishal Rana <vr@labstack.com>
2017-07-07 10:39:58 -0700

    Fixed a race condition
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- logging.go
+++ logging.go
@@ -122,15 +122,12 @@ func (l *Logging) Log(level, format string, args ...interface{}) {
 	}
 
 	if l.timer == nil {
+		l.timer = time.Tick(time.Duration(l.DispatchInterval) * time.Second)
 		go func() {
-			l.timer = time.Tick(time.Duration(l.DispatchInterval) * time.Second)
 			for range l.timer {
-				go func() {
-					if err := l.dispatch(); err != nil {
-						l.logger.Error(err)
-					}
-					l.resetLogs()
-				}()
+				if err := l.dispatch(); err != nil {
+					l.logger.Error(err)
+				}
 			}
 		}()
 	}
