a41026f0cd16c22d8dc57371e2bdaa72aeb04cff
Vishal Rana <vr@labstack.com>
2017-11-28 18:34:29 -0800

    Bumped v0.15.0
    Signed-off-by: Vishal Rana <vr@labstack.com>

---
+++ currency.go
@@ -0,0 +1,28 @@
+package labstack
+
+import "time"
+
+type (
+	CurrencyExchangeRequest struct {
+		Base string `json:"base"`
+	}
+
+	CurrencyExchangeResponse struct {
+		Rates     map[string]float64 `json:"rates"`
+		UpdatedAt time.Time          `json:"updated_at"`
+	}
+)
+
+func (c *Client) CurrencyExchange(req *CurrencyExchangeRequest) (res *CurrencyExchangeResponse, err *APIError) {
+	res = new(CurrencyExchangeResponse)
+	_, e := c.resty.R().
+		SetBody(req).
+		SetResult(res).
+		SetError(err).
+		Post("/currency/exchange")
+	if e != nil {
+		err = new(APIError)
+		err.Message = e.Error()
+	}
+	return
+}
