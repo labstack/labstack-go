298b94646100587bef9d4da747213a6d26309603
Vishal Rana <vr@labstack.com>
2017-12-02 19:52:18 -0800

    Bumped v0.16.0
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- pdf.go
+++ pdf.go
@@ -1,22 +1,83 @@
 package labstack
 
+import "strconv"
+
 type (
 	PDFImageRequest struct {
-		File string
+		File    string
+		Extract bool
 	}
 
 	PDFImageResponse struct {
 		*Download
 	}
+
+	PDFSplitRequest struct {
+		File  string
+		Pages string
+	}
+
+	PDFSplitResponse struct {
+		*Download
+	}
+
+	PDFCompressRequest struct {
+		File    string
+		Quality int
+		DPI     int
+	}
+
+	PDFCompressResponse struct {
+		*Download
+		Size int64 `json:"size"`
+	}
 )
+
+func (c *Client) PDFCompress(req *PDFCompressRequest) (res *PDFCompressResponse, err *APIError) {
+	res = new(PDFCompressResponse)
+	_, e := c.resty.R().
+		SetFile("file", req.File).
+		SetFormData(map[string]string{
+			"quality": strconv.Itoa(req.Quality),
+			"dpi":     strconv.Itoa(req.DPI),
+		}).
+		SetResult(res).
+		SetError(err).
+		Post("/pdf/compress")
+	if e != nil {
+		err = new(APIError)
+		err.Message = e.Error()
+	}
+	return
+}
 
 func (c *Client) PDFImage(req *PDFImageRequest) (res *PDFImageResponse, err *APIError) {
 	res = new(PDFImageResponse)
 	_, e := c.resty.R().
 		SetFile("file", req.File).
+		SetFormData(map[string]string{
+			"extract": strconv.FormatBool(req.Extract),
+		}).
 		SetResult(res).
 		SetError(err).
 		Post("/pdf/image")
+	if e != nil {
+		err = new(APIError)
+		err.Message = e.Error()
+	}
+	return
+}
+
+func (c *Client) PDFSplit(req *PDFSplitRequest) (res *PDFSplitResponse, err *APIError) {
+	res = new(PDFSplitResponse)
+	_, e := c.resty.R().
+		SetFile("file", req.File).
+		SetFormData(map[string]string{
+			"pages": req.Pages,
+		}).
+		SetResult(res).
+		SetError(err).
+		Post("/pdf/split")
 	if e != nil {
 		err = new(APIError)
 		err.Message = e.Error()
