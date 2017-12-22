8ccaea13de336d542b3ba3b37f7135803fe69026
Vishal Rana <vr@labstack.com>
2017-12-21 21:18:40 -0800

    Bumpled v0.17.0
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- image.go
+++ image.go
@@ -14,12 +14,27 @@ type (
 
 	ImageResizeRequest struct {
 		File   string
-		Width  int  `form:"width"`
-		Height int  `form:"height"`
-		Crop   bool `form:"crop"`
+		Width  int
+		Height int
+		Format string
 	}
 
 	ImageResizeResponse struct {
+		*Download
+	}
+
+	ImageWatermarkRequest struct {
+		File     string
+		Text     string
+		Font     string
+		Size     int
+		Color    string
+		Opacity  int
+		Position string
+		Margin   int
+	}
+
+	ImageWatermarkResponse struct {
 		*Download
 	}
 )
@@ -45,11 +60,34 @@ func (c *Client) ImageResize(req *ImageResizeRequest) (res *ImageResizeResponse,
 		SetFormData(map[string]string{
 			"width":  strconv.Itoa(req.Width),
 			"height": strconv.Itoa(req.Height),
-			"crop":   strconv.FormatBool(req.Crop),
+			"format": req.Format,
 		}).
 		SetResult(res).
 		SetError(err).
 		Post("/image/resize")
+	if e != nil {
+		err = new(APIError)
+		err.Message = e.Error()
+	}
+	return
+}
+
+func (c *Client) ImageWatermark(req *ImageWatermarkRequest) (res *ImageWatermarkResponse, err *APIError) {
+	res = new(ImageWatermarkResponse)
+	_, e := c.resty.R().
+		SetFile("file", req.File).
+		SetFormData(map[string]string{
+			"text":     req.Text,
+			"font":     req.Font,
+			"size":     strconv.Itoa(req.Size),
+			"color":    req.Color,
+			"opacity":  strconv.Itoa(req.Opacity),
+			"position": req.Position,
+			"margin":   strconv.Itoa(req.Margin),
+		}).
+		SetResult(res).
+		SetError(err).
+		Post("/image/watermark")
 	if e != nil {
 		err = new(APIError)
 		err.Message = e.Error()
